e474c99e145a14998d3825cdeed5388d12828c59
dinaabuzeena <dinamohammad735@gmail.com>
2021-06-21 09:13:33 +0300

    new file

---
+++ app.js
@@ -0,0 +1,19 @@
+let name ;
+name = prompt("whats your name?")
+alert("wlecome" + name)
+
+
+let age ;
+age = prompt("how old are you?")
+alert("my age" + age)
+
+
+ let myJob ;
+ myJob = prompt("what do you work?")
+ alert("myjob" + myJob)
+ 
+
+ let myFavoriteColor ;
+ myFavoriteColor =prompt("What is your favorite color?")
+ alert("favorite color" + myFavoriteColor)
+ 
